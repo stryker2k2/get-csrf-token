049d77f1e6f27f9542b3ab453252b0507ad61769
stryker2k2 <stryker2k2@msn.com>
2022-01-24 11:22:43 -0600

    Update csrf.py

--- csrf.py
+++ csrf.py
@@ -4,16 +4,18 @@ url = 'https://localhost:8080/login'
 getRequest = requests.get(url, verify=False)
 
 # Get Session Key
+# Assuming "Set-Cookie:session={{ session_key }}; Path=/; HttpOnly;"
 if 'Set-Cookie' in getRequest.headers:
     headText = str(getRequest.headers)
     headLoc = headText.find('session')
     sessionKey = headText[headLoc + 8:headLoc + 119]
 
 # Get CSRF Key
-if 'csrf-token' in getRequest.text:
+# Assuming "<input type="hidden" name="csrf_token" value="{{ csrf_token() }}"/>"
+if 'csrf_token' in getRequest.text:
     csrfText = str(getRequest.text)
     csrfLoc = csrfText.find('csrf')
-    csrfKey = csrfText[csrfLoc + 22:csrfLoc + 113]
+    csrfKey = csrfText[csrfLoc + 19:csrfLoc + 110]
 
 # Print Keys
 print("\nPerforming GET Request on " + url)
